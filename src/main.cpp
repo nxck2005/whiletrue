7b9f2080886932a722606e772eaaf40c5130419d
Nishchal Ravi <nxckmusicprod@gmail.com>
2026-02-28 22:50:04 +0530

    Fix buildings not able to be bought

--- src/main.cpp
+++ src/main.cpp
@@ -295,11 +295,19 @@ int main() {
             else if (ch == ' ') { game.registerClick(); } 
             else if (ch == 'b') { game.buyBuff(); } 
             else if (ch == 'c') { game.buyClickShare(); } 
-            else if (ch == '1') { game.buyBuilding(0); } 
-            else if (ch == '2') { game.buyBuilding(1); } 
-            else if (ch == '3') { game.buyBuilding(2); } 
-            else if (ch == '4') { game.buyBuilding(3); } 
-            else if (ch == '5') { game.buyBuilding(4); } 
+            else if (ch == '0') { game.buyBuilding(0); } 
+            else if (ch == '1') { game.buyBuilding(1); } 
+            else if (ch == '2') { game.buyBuilding(2); } 
+            else if (ch == '3') { game.buyBuilding(3); } 
+            else if (ch == '4') { game.buyBuilding(4); }
+            else if (ch == '5') { game.buyBuilding(5); }
+            else if (ch == '6') { game.buyBuilding(6); }
+            else if (ch == '7') { game.buyBuilding(7); }
+            else if (ch == '8') { game.buyBuilding(8); }
+            else if (ch == '9') { game.buyBuilding(9); }
+            else if (ch == '0') { game.buyBuilding(10); }
+            else if (ch == '-') { game.buyBuilding(11); } 
+            else if (ch == '=') { game.buyBuilding(12); } 
             else if (ch == 's') { game.saveGame(); } 
             else if (ch == 'l') { game.loadGame(); }
             else if (ch == 'g') { game.catchCache(); }
